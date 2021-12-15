3013909de3204771637d11509b4f1d7574ed24bc
Val-Cnn <58478944+Val-Cnn@users.noreply.github.com>
2021-12-15 22:30:38 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "inputs.h"
 #include "kdtree.h"
 #include <time.h>
+#include "convexHull.h"
 
 #define SHOW 1
 
@@ -75,6 +76,87 @@ int main()
 	printf("elapsed time : %ld seconds\n", tac-tic);
 
 	free_kd_tree(head);
+	
+	
+	
+	
+	
 
 	return EXIT_SUCCESS;
 }
+
+
+
+/*
+
+int main() // FOR CONVEX HULL
+{
+	// give a bit of entropy for the seed of rand()
+	// or it will always be the same sequence
+	int seed = (int) time(NULL);
+	srand(seed);
+
+	// we print the seed so you can get the distribution of points back
+	printf("seed=%d\n", seed);
+
+	bov_window_t* window = bov_window_new(800, 800, "My first BOV program");
+	bov_window_set_color(window, (GLfloat[]){0.9f, 0.85f, 0.8f, 1.0f});
+
+	const GLsizei nPoints = 100000;
+	GLfloat (*coord)[2] = malloc(sizeof(coord[0])*nPoints);
+    #if 0 // put 1 for random polygon
+        random_polygon(coord, nPoints, 4);
+    #else
+        random_points(coord, nPoints);
+    #endif
+
+    GLfloat (*L)[2] = malloc(sizeof(L)*(nPoints));
+
+    for (int j = 0; j<nPoints;j++){
+        L[j][0] = coord[j][0];
+        L[j][1] = coord[j][1];
+    }
+    int a1 = coordon(nPoints, L);
+    int numb = a1;
+    printf("%d \n", numb);
+
+
+
+
+
+
+
+	bov_points_t *coordDraw = bov_points_new(coord, nPoints, GL_STATIC_DRAW);
+    bov_points_t *cool = bov_points_new(L, numb, GL_STATIC_DRAW);
+	bov_points_set_color(coordDraw, (GLfloat[4]) {0.0, 0.0, 0.0, 1.0});
+	bov_points_set_outline_color(coordDraw, (GLfloat[4]) {0.3, 0.12, 0.0, 0.25});
+
+	while(!bov_window_should_close(window)){
+		bov_points_set_width(coordDraw, 0.003);
+		bov_points_set_outline_width(coordDraw, 0.002);
+
+        bov_points_set_width(cool, 0.003);
+        bov_points_set_outline_width(cool, 0.002);
+        bov_line_loop_draw(window, cool, 0, numb);
+
+
+		// points_set_width(coordDraw, 0.003);
+		bov_points_set_outline_width(coordDraw, -1.);
+		bov_points_draw(window, coordDraw, 0, nPoints);
+
+
+        bov_points_set_outline_width(cool, -1.);
+        bov_points_draw(window, cool, 0, numb);
+
+		bov_window_update(window);
+	}
+
+	bov_points_delete(coordDraw);
+	free(coord);
+	bov_window_delete(window);
+
+	return EXIT_SUCCESS;
+}
+
+
+*/
