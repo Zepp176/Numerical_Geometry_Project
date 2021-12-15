002443f30f957ddd2b3ac390efefee0f1fd24045
Val-Cnn <58478944+Val-Cnn@users.noreply.github.com>
2021-12-15 23:44:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -89,7 +89,6 @@ int main()
 
 
 /*
-
 int main() // FOR CONVEX HULL
 {
 	// give a bit of entropy for the seed of rand()
@@ -103,7 +102,7 @@ int main() // FOR CONVEX HULL
 	bov_window_t* window = bov_window_new(800, 800, "My first BOV program");
 	bov_window_set_color(window, (GLfloat[]){0.9f, 0.85f, 0.8f, 1.0f});
 
-	const GLsizei nPoints = 100000;
+	const GLsizei nPoints = 1000000;
 	GLfloat (*coord)[2] = malloc(sizeof(coord[0])*nPoints);
     #if 0 // put 1 for random polygon
         random_polygon(coord, nPoints, 4);
@@ -132,22 +131,37 @@ int main() // FOR CONVEX HULL
 	bov_points_set_color(coordDraw, (GLfloat[4]) {0.0, 0.0, 0.0, 1.0});
 	bov_points_set_outline_color(coordDraw, (GLfloat[4]) {0.3, 0.12, 0.0, 0.25});
 
+    bov_points_set_color(cool, (GLfloat[4]) {0.0, 0.0, 0.0, 1.0});
+    bov_points_set_outline_color(cool, (GLfloat[4]) {1.0, 0.0, 0.0, 0.0});
+
+
+    double old_time;
+    old_time = bov_window_get_time(window);
+    int count = 0;
 	while(!bov_window_should_close(window)){
 		bov_points_set_width(coordDraw, 0.003);
 		bov_points_set_outline_width(coordDraw, 0.002);
 
         bov_points_set_width(cool, 0.003);
         bov_points_set_outline_width(cool, 0.002);
-        bov_line_loop_draw(window, cool, 0, numb);
 
+
+        if (bov_window_get_time(window) - old_time > 1) {
+            old_time = bov_window_get_time(window);
+            if(count<numb) {
+                count++;
+            }
+        }
+        bov_line_strip_draw(window, cool, 0, count);
+        bov_points_set_width(cool, 0.01);
+        bov_points_draw(window, cool, 0, count);
 
 		// points_set_width(coordDraw, 0.003);
 		bov_points_set_outline_width(coordDraw, -1.);
 		bov_points_draw(window, coordDraw, 0, nPoints);
 
 
-        bov_points_set_outline_width(cool, -1.);
-        bov_points_draw(window, cool, 0, numb);
+
 
 		bov_window_update(window);
 	}
@@ -158,6 +172,5 @@ int main() // FOR CONVEX HULL
 
 	return EXIT_SUCCESS;
 }
-
 
 */
