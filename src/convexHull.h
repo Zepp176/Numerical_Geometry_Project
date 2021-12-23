d9b08afd62ec7e29c1f52af8e7305aacecf56f40
Val-Cnn <58478944+Val-Cnn@users.noreply.github.com>
2021-12-23 11:35:22 +0100

    Update convexHull.h

--- src/convexHull.h
+++ src/convexHull.h
@@ -3,5 +3,5 @@
 
 int righturn(double x1, double x2, double x3, double y1, double y2, double y3 );
 static int compare_x(const void *a_v, const void *b_v);
-int* coordonstep(GLsizei nPoints, GLfloat L[][2], GLfloat step[][2]);
+int* coordonStep(GLsizei nPoints, GLfloat L[][2], GLfloat step[][2]);
 int coordon(GLsizei nPoints, GLfloat L[][2]);
